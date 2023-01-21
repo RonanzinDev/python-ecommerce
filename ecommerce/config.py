02d528d1755ce062a29c2fe230fe85e745452eb3
Gabriel Morelli <gabriel.morelli@codenapp.com>
2023-01-21 13:05:31 -0300

    Config db

---
+++ ecommerce/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+APP_ENV = os.getenv('APP_ENV', 'development')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'root')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'fastapi_ecommerce')
+TEST_DATABASE_NAME = os.getenv('DATABASE_NAME', 'test_fastapi_ecommerce')
+# REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
+# REDIS_PORT = os.getenv('REDIS_PORT', '6379')
+# REDIS_DB = os.getenv('REDIS_DB', '0' if APP_ENV == 'TESTING' else '0')
